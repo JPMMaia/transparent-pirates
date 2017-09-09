c4ea5d46dbb005beba8d644b053579119249d78f
João Neto <joao.neto.feup@gmail.com>
2017-09-09 21:53:54 +0100

    Werewolf
    Signed-off-by: João Neto <joao.neto.feup@gmail.com>

--- Assets/AI/RotatePlayer2.cs
+++ Assets/AI/RotatePlayer2.cs
@@ -3,22 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotatePlayer2 : MonoBehaviour {
-
-    Transform target;
-
-    private void Start()
-    {
-        target = GetComponentInParent<AIPath>().target;
-    }
+    
 
     // Update is called once per frame
     void LateUpdate () {
         transform.rotation = Quaternion.Euler(0, 0, 0);
 
-        if (GetComponent<AIPath>().velocity2D.x < 0.1)
-            transform.localScale = new Vector3(.5f,.5f,.5f);
-        else
-            transform.localScale = new Vector3(-.5f, .5f, .5f);
+        if (GetComponentInParent<AIPath>().velocity2D.magnitude > .05)
+        {
+            if (GetComponentInParent<AIPath>().velocity2D.x < 0)
+                transform.localScale = new Vector3(.5f, .5f, .5f);
+            else
+                transform.localScale = new Vector3(-.5f, .5f, .5f);
+        }
 
     }
     
--- Assets/Scripts/Terrain/Maps/Map.cs
+++ Assets/Scripts/Terrain/Maps/Map.cs
@@ -5,11 +5,14 @@ namespace Assets.Scripts.Terrain
 {
     public class Map : MonoBehaviour
     {
+        AstarPath path;
+
         private char[,] _map;
 
         public void Start()
         {
             IMapBuilder builder = new DefaultMapBuilder();
+
 
             _map = builder.Build();
 
@@ -29,10 +32,12 @@ namespace Assets.Scripts.Terrain
             builders[' '] = new BlankTileBuilder();
 
             var parent = this.transform;
+            var rowIndex = 0;
+            var columnIndex = 0;
 
-            for(var rowIndex = 0; rowIndex < map.GetLength(0); ++rowIndex)
+            for(rowIndex = 0; rowIndex < map.GetLength(0); ++rowIndex)
             {
-                for (var columnIndex = 0; columnIndex < map.GetLength(1); ++columnIndex)
+                for (columnIndex = 0; columnIndex < map.GetLength(1); ++columnIndex)
                 {
                     var position = new Vector3(columnIndex, -rowIndex, 0.0f);
                     var rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
@@ -42,6 +47,11 @@ namespace Assets.Scripts.Terrain
                     tileBuilder.Build(tileInstance);
                 }
             }
+            
+            AstarPath.active.data.gridGraph.SetDimensions(columnIndex*2, rowIndex*2,.5f);
+            AstarPath.active.data.gridGraph.center = new Vector3(Mathf.Floor(columnIndex/2), -Mathf.Floor(rowIndex/2), 0);
+
+            AstarPath.active.Scan();
         }
     }
 }
