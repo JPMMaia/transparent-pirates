71d8298a65ac1488b5f1e37e35bd5fd7009c33cc
jpmmaia <jpmmaia@gmail.com>
2017-09-09 23:23:03 +0100

    - Creating bullet prefab.

---
+++ Assets/Scripts/Actors/Weapons/Pistols/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Actors.Weapons.Pistols
+{
+    public class Bullet : MonoBehaviour, IDamager
+    {
+        public uint Damage
+        {
+            get
+            {
+                return _damage;
+            }
+        }
+
+        private uint _damage;
+    }
+}
