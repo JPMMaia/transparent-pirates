ce0f7275ab5ef2669bdbac9af2672ed29a08ce56
João Neto <joao.neto.feup@gmail.com>
2017-09-09 16:02:17 +0100

    Reimported CharacterScene.unity
    Signed-off-by: João Neto <joao.neto.feup@gmail.com>

---
+++ Assets/Players/Player1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+
+    public float maxSpeed = 5f;
+    Vector2 move;
+    Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float hSpeed = Input.GetAxis("Horizontal");
+        float vSpeed = Input.GetAxis("Vertical");
+
+        move = new Vector2(hSpeed, vSpeed);
+
+        rigidBody.velocity = move * maxSpeed;
+    }
+}
