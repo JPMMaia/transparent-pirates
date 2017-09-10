5ff28987064b0c57488d583dd9f42e8c19b1499d
JoaoNeto <joao.neto.feup@gmail.com>
2017-09-10 13:33:16 +0100

    - gameover

---
+++ Assets/RetryFunc.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryFunc : MonoBehaviour {
+    public void oneclick() {
+        SceneManager.LoadScene("MainMenu");
+    }
+	
+}
