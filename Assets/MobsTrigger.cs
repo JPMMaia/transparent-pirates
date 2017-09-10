ef95301d34b639bb374283df9eb9f437136c83be
João Neto <joao.neto.feup@gmail.com>
2017-09-10 09:24:11 +0100

    - adding uncommitted files

---
+++ Assets/MobsTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobsTrigger : MonoBehaviour {
+
+    public List<EnemyAI> mobs = new List<EnemyAI>();
+
+    public void Awake()
+    {
+        foreach (EnemyAI e in mobs)
+            e.Deactivate();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            foreach (EnemyAI e in mobs)
+                e.Activate();
+
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+}
