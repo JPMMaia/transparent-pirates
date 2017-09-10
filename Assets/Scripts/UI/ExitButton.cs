d885966b6d3385a4eba0babf0c1638309ce6d291
jpmmaia <jpmmaia@gmail.com>
2017-09-10 09:04:32 +0100

    - Adding exit button script.

---
+++ Assets/Scripts/UI/ExitButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class ExitButton : MonoBehaviour
+    {
+        public void OnClicked()
+        {
+            Application.Quit();
+        }
+    }
+}
